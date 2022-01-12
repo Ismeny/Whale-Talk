229a0936107194a13589f0ea5d61c49adfb30e0a
Ismeny Saguilan <menysag2@gmail.com>
2022-01-12 14:32:17 -0600

    creating variables

---
+++ main.js
@@ -0,0 +1,8 @@
+// variable that holds the text you want to translate
+const input = 'i love my husband';
+// variable that holds the vowels excluding y
+const vowels = ['a','e','i','o','u'];
+// variable that will hold vowels from the input string
+let resultArray = [];
+
+
